c8478eaffd46869eafacd12809a0706fe161b148
PamelaRomero <62120800+PamelaRomero@users.noreply.github.com>
2025-07-22 16:43:56 +0200

    Update MDI_permutation.py

--- MDI_permutation.py
+++ MDI_permutation.py
@@ -16,9 +16,9 @@ from utils_multi_step_prediction import Dataset, Level_min, SplitTrainValidTest,
 
 
 def MDI(regressor, feature_names, folder_save, name_save):
-    forest_importances = pd.Series(regressor.feature_importances_, index=feature_names)  
+    features_importances = pd.Series(regressor.feature_importances_, index=feature_names)  
     fig, ax = plt.subplots(figsize=(14, 4))
-    forest_importances.plot.bar(ax=ax)
+    features_importances.plot.bar(ax=ax)
     ax.set_title("Feature importances using MDI")
     ax.set_ylabel("Mean decrease in impurity")
     fig.tight_layout()
@@ -30,10 +30,10 @@ def Permutation(regressor, X_test, y_test, feature_names, folder_save, name_save
     result = permutation_importance(
         regressor, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2
     )   
-    forest_importances = pd.Series(result.importances_mean, index=feature_names)
+    features_importances = pd.Series(result.importances_mean, index=feature_names)
 
     fig, ax = plt.subplots(figsize=(14, 4))
-    forest_importances.plot.bar(yerr=result.importances_std, ax=ax)
+    features_importances.plot.bar(yerr=result.importances_std, ax=ax)
     ax.set_title("Feature importances using permutation")
     ax.set_ylabel("Mean accuracy decrease")
     fig.tight_layout()
@@ -43,8 +43,8 @@ def Permutation(regressor, X_test, y_test, feature_names, folder_save, name_save
 
 
 start = time.time()
-path = '/home/pamela/Documents/FRANCIA/GitHub/NewNewVersionGitHub/Datanew'
-folder = "/home/pamela/Desktop/Codigos_paper/"
+path = '/home/.../Data'
+folder = "/home/.../"
 percentage = 0.5
 train_pc, valid_pc, test_pc = 0.7, 0.1, 0.2
 
@@ -75,7 +75,7 @@ vec_tst = np.array(Vector(time_tst, x_data_tst, D_tst, L_tst, R_tst, P_tst), dty
 
 feature_names = ['Dose','L_0','L_1','L_2','L_3','R_0','R_1','R_2','R_3','R_4','R_5','P_0','P_1','P_2','P_3','P_4','P_5','P_6','P_7','P_8','T_0','T_1','T_2','T_3','T_4','T_5','T_6','T_7','T_8','T_9','T_10','T_11','T_12','T_13','T_14','T_15','T_16','T_17','T_18','T_19','T_20','T_21','T_22','T_23','T_24','T_25','T_26','T_27','T_28','T_29','T_30','T_31','S_0','S_1','S_2','S_3','S_4','S_5','S_6','S_7','S_8','S_9','S_10','S_11','S_12','S_13','S_14','S_15','S_16','S_17','S_18','S_19','S_20','S_21','S_22','S_23','S_24','S_25','S_26','S_27','S_28','S_29','S_30','S_31']
 
-folder_save = '/home/pamela/Desktop/Codigos_paper/'
+folder_save = "/home/.../"
 
 regressor1 = DecisionTreeRegressor(max_depth=10, min_samples_leaf=1, random_state=0)
 regressor1.fit(vec_trn, y_trn)
@@ -87,7 +87,7 @@ regressor2.fit(vec_trn, y_trn)
 MDI(regressor2, feature_names, folder_save, 'MDI_RandomForest')
 Permutation(regressor2, vec_tst, y_tst, feature_names, folder_save, 'Permutation_RandomForest')
 
-regressor3 = xgb.XGBRegressor(max_depth=3, n_estimators=10, learning_rate=0.5)  #n_estimators=10, max_depth=10
+regressor3 = xgb.XGBRegressor(max_depth=3, n_estimators=10, learning_rate=0.5)  
 regressor3.fit(vec_trn, y_trn)
 MDI(regressor3, feature_names, folder_save, 'MDI_XGB')
 Permutation(regressor3, vec_tst, y_tst, feature_names, folder_save, 'Permutation_XGB')
